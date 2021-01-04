00788a0e93c8cdc9649ff9740ecf51d29f570e8a
webdevelopermohiuddin <mohiuddinmazumder94@gmail.com>
2021-01-05 01:26:22 +0600

    search implement

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,17 +4,37 @@ import Countries from '../Countries/Countries';
 
 const Home = () => {
     const [countries, setCountries] = useState([])
+    const [Search, setSearch] = useState('')
+    const [filterCountries, setFilterCountries] = useState([]);
+
     useEffect(() =>{
         fetch('https://restcountries.eu/rest/v2/all')
         .then(res => res.json())
         .then(data => setCountries(data))
     },[])
+
+    useEffect(() =>{
+      setFilterCountries(
+        countries.filter(country => {
+                return country.name.toLowerCase().includes(Search.toLowerCase())
+             })
+      )
+    }, [Search, countries])
+
+    // const filterCountries = countries.filter(country => {
+    //     return country.name.toLowerCase().includes(Search.toLowerCase())
+    // })
     return (
         <>
         <Container>
-            <Row className="justify-content-center">
+            <Row>
+            <input type="text" placeholder="Search here" onChange={e => setSearch(e.target.value)}/>
+            <button>asia</button> <button>europe </button> <button>africa</button>
+            </Row>
+            
+            <Row className="justify-content-center ">
                 {
-                    countries.map(country => <Countries country={country} key={country.alpha3Code}/>)
+                    filterCountries.map(country => <Countries country={country} key={country.alpha3Code}/>)
                 }
             </Row>
         </Container>
